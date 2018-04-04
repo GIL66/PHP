336c39a34b3b8ce0dc32e31036b73f439afd88a7
Bob Marley <bob@marley.com>
2018-04-04 14:34:45 +0200

    inclusion du header

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,10 @@
 <header>
 <nav>
 <ul>
-<li><a href="index.php?page=home"></a>Accueil</li>
-<li><a href="index.php?page=registration"></a>Inscription</li>
-<li><a href="index.php?page=contact"></a>Contact</li>
-<li><a href="index.php?page=news"></a>News</li>
+<li><a href="index.php?page=home">Accueil</a></li>
+<li><a href="index.php?page=registration">Inscription</a></li>
+<li><a href="index.php?page=contact">Contact</a></li>
+<li><a href="index.php?page=news">News</a></li>
 </ul>
 </nav>
 </header>
