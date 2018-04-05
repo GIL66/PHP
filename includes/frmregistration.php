a1980b4bff02687b2f7b690a4136fcb7e5cff2cb
Bob Marley <bob@marley.com>
2018-04-05 13:38:18 +0200

    formulaire inscription

--- includes/frmregistration.php
+++ includes/frmregistration.php
@@ -1,19 +1,19 @@
 <form method="post" action="#">
 <div>
 <label for="nom">Nom&nbsp;:</label>
-<input type ="text" name="nom" />
+<input type ="text" name="nom" value="<?php if(isset($nom)) echo $nom; ?>" />
 </div>
 <div>
 <label for="prenom">Prénom&nbsp;:</label>
-<input type ="text" name="prenom" />
+<input type ="text" name="prenom" value="<?php if(isset($prenom)) echo $prenom; ?>"/>
 </div>
 <div>
 <label for="mail">Mail&nbsp;:</label>
-<input type ="text" name="mail" />
+<input type ="mail" name="mail" value="<?php if(isset($mail)) echo $mail; ?>"/>
 </div>
 <div>
 <label for="mdp">Mot de passe&nbsp;:</label>
-<input type ="text" name="mdp" />
+<input type ="password" name="mdp" />
 </div>
 <div>
 <input type="reset" value="Effacer" />
