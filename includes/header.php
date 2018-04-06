7bdd8f6de8240fb0d0e184b249be30ac12b53338
Bob Marley <bob@marley.com>
2018-04-06 16:13:52 +0200

    ajout de la page login

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,7 @@
 <nav>
 <ul>
 <li><a href="index.php?page=home">Accueil</a></li>
+<li><a href="index.php?page=login">Connexion</a></li>
 <li><a href="index.php?page=registration">Inscription</a></li>
 <li><a href="index.php?page=contact">Contact</a></li>
 <li><a href="index.php?page=news">News</a></li>
