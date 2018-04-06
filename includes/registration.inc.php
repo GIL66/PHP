93da36225344b1e04894ffdde8fddfff53f71c0f
Bob Marley <bob@marley.com>
2018-04-06 09:23:42 +0200

    insertion du script de connexion à la bdd

--- includes/registration.inc.php
+++ includes/registration.inc.php
@@ -1,54 +1,74 @@
 <h1>Inscription</h1>
 <?php
 if(isset($_POST['frmregistration'])) {
-    
-    //syntaxe classique
-   /* if (isset($_POST['nom'])) {
-        $nom = $_POST['nom'];
-    }
-    else {
-        $nom = "";
-    }
 
-    // opérateur ternaire
-    $nom = isset($_POST['nom']) ? $_POST['nom'] : "";*/
+//syntaxe classique
+/* if (isset($_POST['nom'])) {
+$nom = $_POST['nom'];
+}
+else {
+$nom = "";
+}
 
-  // Opérateur Null coalescent PHP 7
-    $nom = $_POST['nom'] ?? "";
-    $prenom = $_POST['prenom'] ?? "";
-    $mail = $_POST['mail'] ?? "";
-    $mdp = $_POST['mdp'] ?? "";
+// opérateur ternaire
+$nom = isset($_POST['nom']) ? $_POST['nom'] : "";*/
 
-    $erreur = array();
+// Opérateur Null coalescent PHP 7
+$nom = $_POST['nom'] ?? "";
+$prenom = $_POST['prenom'] ?? "";
+$mail = $_POST['mail'] ?? "";
+$mdp = $_POST['mdp'] ?? "";
 
-    if ($nom == "") array_push($erreur, "Veuillez saisir votre nom");
-    if ($prenom == "") array_push($erreur, "Veuillez saisir votre prenom");
-    if ($mail == "") array_push($erreur, "Veuillez saisir votre mail");
-    if ($mdp == "") array_push($erreur, "Veuillez saisir votre mot de passe");
+$erreur = array();
 
-    if (count($erreur) > 0) {
-        $message = "<ul>";/*
+if ($nom == "") array_push($erreur, "Veuillez saisir votre nom");
+if ($prenom == "") array_push($erreur, "Veuillez saisir votre prenom");
+if ($mail == "") array_push($erreur, "Veuillez saisir votre mail");
+if ($mdp == "") array_push($erreur, "Veuillez saisir votre mot de passe");
+
+if (count($erreur) > 0) {
+$message = "<ul>";/*
 for($i = 0 ; $i<count($erreur) ; $i++){  
-    $message .= "<li>";
-    $message .= $erreur[$i];
-    $message .= "</li>";
+$message .= "<li>";
+$message .= $erreur[$i];
+$message .= "</li>";
 }
 $message = $message . "</ul>";
 echo $message;*/
 foreach($erreur as $ligneMessage) {
-    $message .= "<li>";
-    $message .= $ligneMessage;
-    $message .= "</li>";
+$message .= "<li>";
+$message .= $ligneMessage;
+$message .= "</li>";
 }
 $message .= "</ul>";
 echo $message;
 include "frmregistration.php";
-    }
-    else {
- echo "pas d'erreur";
-    }
 }
 else {
-    echo "Je ne viens pas du formulaire";
+// adresse, utilisateur, mdp, bdd
+$mdp = sha1($mdp); 
+$connection = mysqli_connect("localhost", "root", "", "phpdieppe" );
+$requete = "INSERT INTO T_USERS
+    (USENAME, USEFIRSTNAME, USEMAIL, USEPASSWORD, ID_ROLE)
+    VALUES ('$nom', '$prenom', '$mail', '$mdp', 3)";
+
+if (!$connection) {
+die("Erreur MySQL" . mysqli_connect_errno() . " | " . mysqli_connect_error());
+}
+else {
+
+if(mysqli_query($connection, $requete)) {
+    echo "données enregistrées";
+}
+else {
+    echo "Erreur";
     include "frmregistration.php";
+}
+mysqli_close($connection);
+}
+}
+}
+else {
+echo "Je ne viens pas du formulaire";
+include "frmregistration.php";
 }
